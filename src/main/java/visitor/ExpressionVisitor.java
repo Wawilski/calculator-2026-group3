7565aa67558f22c7e8f8d647d1fd52f47452dc27
wawilski <karpinskiwal@gmail.com>
2026-03-12 15:06:03 +0100

    Visitor Setup for parser

---
+++ src/main/java/visitor/ExpressionVisitor.java
@@ -0,0 +1,28 @@
+
+package visitor;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import calculator.Expression;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import calculator.Notation;
+import calculator.Operation;
+
+/**
+ * EquationListener
+ */
+
+public class ExpressionVisitor extends calculatorBaseVisitor<Operation> {
+
+}
