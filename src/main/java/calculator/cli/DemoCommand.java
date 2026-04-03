7c781cba7e7300725c47b9c7fee455d23cfaaf98
oussama <oussama@oussama.umons.ac.be>
2026-04-03 19:57:42 +0200

    add CLI mode

---
+++ src/main/java/calculator/cli/DemoCommand.java
@@ -0,0 +1,19 @@
+package calculator.cli;
+
+public class DemoCommand implements CliCommand{
+
+    @Override
+    public String name() {
+        return "demo";
+    }
+
+    @Override
+    public String description() {
+        return "Start of demo...";
+    }
+
+    @Override
+    public CommandResult execute(CliContext context, String rawInput) {
+        return CommandResult.ok("This is a simple demo...");
+    }
+}
